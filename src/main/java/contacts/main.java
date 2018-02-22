cbe306293c291b7bf262bd5b4f96cb921c45dd56
Daniel (Chuan) Sha <chuanhui.sha@stonybrook.edu>
2018-02-22 13:33:13 -0500

    starting to integrate elasticsearch

--- src/main/java/contacts/main.java
+++ src/main/java/contacts/main.java
@@ -2,6 +2,9 @@ package contacts;
 
 import static spark.Spark.*;
 import org.apache.log4j.BasicConfigurator;
+import org.elasticsearch.*;
+import org.elasticsearch.node.Node;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 /*
@@ -9,20 +12,23 @@ import com.fasterxml.jackson.databind.ObjectMapper;
  * 
  * 	****Notes in order of importance****
  * 
- * 	1) Testing was done using "curl". This Java App works under the assumption that curl testing is accurate and fulfills the challenge's requirements.
+ * 	1) Elasticsearch now has their own java rest api, using it seems to defeat the purpose of this exercise so I did not.
+ * 		- However a large portion of their documentation for Java is outdated. Nodes and transportclients have been deprecated to promote their own Java API.
+ * 		- So I built this first using an arrayList/JSON instead of Indexes and attempted to integrate Elasticsearch afterwards.
+ * 	2) Testing was done using "curl". This Java App works under the assumption that curl testing is accurate and fulfills the challenge's requirements.
  * 		- 2 Examples of curl command (assuming cwd is curl.exe folder like "D:\code\curl\curl-7.58.0-win64-mingw\bin"
  * 		- $ ./curl.exe http://localhost:8081/hello
  * 		- $ ./curl.exe -X POST -d "name=frankStalone&address=PalmerSt" http://localhost:8081/contact
  *		- Side note* newest windows powershell created an alias for "curl" to invoke "Invoke-WebRequest" instead. Call curl.exe to resolve.
- * 	2) post uses queryParams {name & address & email & number} to resolve.
+ * 	3) post uses queryParams {name & address & email & number} to resolve.
  * 		- Must provide name queryParam. All others are optional.
  * 		- Not sure if this is correct, but google doc didn't specify a method to require these attributes.
  * 		- Will still create contact if any of three attributes besides name has length > reasonable amount, but will be set to N/A
- * 	3) get/put/delete "/contact/:name" might not be able to handle names with spaces. 
+ * 	4) get/put/delete "/contact/:name" might not be able to handle names with spaces. 
  * 		- $ ./curl.exe http://localhost:8081/contact/frank Stalone
  * 		- Command above will fail in powershell with message "can not resolve host"
- * 	4) port can be changed by changing the variable "portToUse" near the top of this class file.
- * 	5) BasicConfigurator was imported to resolve an error. I still don't quite understand it to be honest.
+ * 	5) port can be changed by changing the variable "portToUse" near the top of this class file.
+ * 	6) BasicConfigurator was imported to resolve an error. I still don't quite understand it to be honest.
  * 	 
  * 
  */
@@ -53,6 +59,14 @@ public class main {
         //required handlers for Address Book
         //GET ALL + Query
         get("/contact", (req, res)-> {
+        	
+        	String def = "N/A";
+        	String defSize = "10";
+        	String defOffset = "0";
+        	String pageSize = req.queryParamOrDefault("pageSize", defSize);
+        	String pageOffset = req.queryParamOrDefault("pageOffset", defOffset);
+        	String query = req.queryParamOrDefault("query", def);
+        	
             return "PLACEHOLDER PLACEHOLDER PLACEHOLDER PLACEHOLDER";
         });
         
